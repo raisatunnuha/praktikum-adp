d69df2035cc5f5069d712d9936ff6a49f8a0a303
raisatunnuha <162274013+raisatunnuha@users.noreply.github.com>
2024-05-29 08:12:34 +0700

    Add files via upload

---
+++ Raisa-tugas9.py
@@ -0,0 +1,37 @@
+import os
+os.system('cls')
+from termcolor import colored, cprint
+for i in range (2) :
+    cprint(" "*5, 'black', 'on_red', end='')
+    print(' '*3, end='')
+    cprint(' '*5, 'black', 'on_red', end ='')
+    print(' '*3, end='')
+    cprint(' '*5, 'black', 'on_red', end='')
+    print(' '*3, end='')
+    cprint(' '*5, 'black', 'on_red', end='')
+    print(' '*3,end='')
+    cprint(' '*14, 'black', 'on_red')
+for i in range (1) :
+    cprint(" "*5, 'black', 'on_red', end='')
+    print(' '*3, end='')
+    cprint(' '*5, 'black', 'on_red', end ='')
+    print(' '*3, end='')
+    cprint(' '*5, 'black', 'on_red')
+for i in range (4) :
+    cprint(" "*5, 'black', 'on_red', end='')
+    print(' '*3, end='')
+    cprint(' '*5, 'black', 'on_red', end ='')
+    print(' '*3, end='')
+    cprint(' '*5, 'black', 'on_red', end='')
+    print(' '*3, end='')
+    cprint(' '*5, 'white', 'on_dark_grey', end='')
+    print(' '*3,end='')
+    cprint(' '*5, 'black', 'on_red')
+for i in range (3) :
+    cprint(" "*5, 'black', 'on_red', end='')
+    print(' '*11, end='')
+    cprint(' '*5, 'black', 'on_red', end='')
+    print(' '*3,end='')
+    cprint(' '*5, 'white', 'on_dark_grey', end='')
+    print(' '*3,end='')
+    cprint(' '*5, 'black', 'on_red')
